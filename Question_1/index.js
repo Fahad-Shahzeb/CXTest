0c53f70f3c1c1c090d3194b43a8399e89b6052c9
Fahad-Shahzeb <fahadshahzeb1@gmail.com>
2024-02-02 12:35:21 +0500

    Question # 1 Completed

---
+++ Question_1/index.js
@@ -0,0 +1,13 @@
+var sortArray = function (arr) {
+    for (var i = 0; i < arr.length - 1; i++) {
+        for (var j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] < arr[j + 1]) {
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+};
+console.log(sortArray([5, 3, 9, 2, 1]));
